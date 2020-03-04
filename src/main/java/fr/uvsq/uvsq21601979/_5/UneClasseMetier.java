54c3d50d293c6d3423cca90b68286fef07dd3716
Tanguy Culerier <culerier.tanguy@gmail.com>
2020-03-04 10:21:35 +0100

    ajout de la classe UneClasseMetier

---
+++ src/main/java/fr/uvsq/uvsq21601979/_5/UneClasseMetier.java
@@ -0,0 +1,13 @@
+package fr.uvsq.uvsq21601979._5;
+
+import java.time.LocalDateTime;
+
+public class UneClasseMetier
+{
+	public void uneMethodeMetier()
+	{
+		System.out.println(LocalDateTime.now() + "début de uneMethodeMetier");
+		
+		System.out.println(LocalDateTime.now() + "fin de uneMethodeMetier");
+	}
+}
