01c1c813f74061267257e406f420b13d7ba567e5
Culerier Tanguy <culerier.tanguy@gmail.com>
2020-04-01 17:51:54 +0200

    correction checkstyle

---
+++ src/main/java/fr/uvsq/uvsq21601979/_5/Main.java
@@ -0,0 +1,17 @@
+package fr.uvsq.uvsq21601979._5;
+/**
+ * Class main.
+ * @author Tanguy
+ */
+public class Main {
+
+    /**
+     * methode main.
+     * @param args pas d'argument néssésaire dans ce programme
+     */
+    public static void main(final String[] args) {
+        org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger("test");
+        UneClasseMetier.uneMethodeMetier(logger);
+    }
+
+}
--- src/main/java/fr/uvsq/uvsq21601979/_5/UneClasseMetier.java
+++ src/main/java/fr/uvsq/uvsq21601979/_5/UneClasseMetier.java
@@ -1,12 +1,17 @@
 package fr.uvsq.uvsq21601979._5;
 
 import java.time.LocalDateTime;
-
-public class UneClasseMetier
-{
-	public static void uneMethodeMetier(final org.slf4j.Logger l)
-	{
-		l.info(LocalDateTime.now() + "début de uneMethodeMetier");
-		l.info(LocalDateTime.now() + "fin de uneMethodeMetier");
-	}
+/**
+ * @author Tanguy
+ */
+public class UneClasseMetier {
+    /**
+     * methode qui affiche des information dans le
+     * terminal avec la bilbiothèque slf4j.
+     * @param l logger
+     */
+    public static void uneMethodeMetier(final org.slf4j.Logger l) {
+        l.info(LocalDateTime.now() + "début de uneMethodeMetier");
+        l.info(LocalDateTime.now() + "fin de uneMethodeMetier");
+    }
 }
